90c53416ffb2d3d5e664f0fa8084876261268ae1
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-02-17 17:55:01 -0300

    finish project

--- src/model/services/ContractService.java
+++ src/model/services/ContractService.java
@@ -1,6 +1,9 @@
 package model.services;
 
 import model.entities.Contract;
+import model.entities.Installment;
+
+import java.time.LocalDateTime;
 
 public class ContractService {
     private OnlinePaymentService onlinePaymentService;
@@ -10,6 +13,13 @@ public class ContractService {
     }
 
     public void processContract(Contract contract, int months) {
-
+        double basicQuota = contract.getTotalValue() / months;
+        for (int i = 1 ; i <= months ; i++) {
+            LocalDateTime dueDate = contract.getDate().plusMonths(i);
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + interest + fee;
+            contract.getInstallments().add(new Installment(dueDate, quota));
+        }
     }
 }
--- src/model/services/PaypalService.java
+++ src/model/services/PaypalService.java
@@ -1,11 +1,17 @@
 package model.services;
 
 public class PaypalService implements OnlinePaymentService {
+
+    private static final double FEE_PERCENTAGE = 0.02;
+    private static final double MONTHLY_INTEREST = 0.01;
+
+    @Override
     public double paymentFee(double amount) {
-        return 0.02 * amount;
+        return amount * FEE_PERCENTAGE;
     }
 
+    @Override
     public double interest(double amount, int months) {
-        return amount * months / 100;
+        return amount * MONTHLY_INTEREST * months;
     }
 }
