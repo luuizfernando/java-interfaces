9f90df777107b53420b589884bfb90efb8250dac
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-02-17 15:32:53 -0300

    fix/ OnlinePaymentService

--- src/model/services/OnlinePaymentService.java
+++ src/model/services/OnlinePaymentService.java
@@ -1,11 +1,6 @@
 package model.services;
 
 public interface OnlinePaymentService {
-    default double paymentFee(double amount) {
-        return 0.02 * amount;
-    }
-
-    default double interest(double amount, int months) {
-        return months * amount / 100;
-    }
+    double paymentFee(double amount);
+    double interest(double amount, int months);
 }
